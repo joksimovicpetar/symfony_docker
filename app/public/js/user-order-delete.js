873894fe053a1c2b1d19f5d33a923c9576d3f781
joksimovicpetar <petar.d.joksimovic@gmail.com>
2022-11-02 14:11:29 +0100

    order commit

--- app/public/js/user-order-delete.js
+++ app/public/js/user-order-delete.js
@@ -1,23 +1,34 @@
 async function deleteOrderItem(routeEdit, dataId) {
     console.log(dataId);
     try{
-        await fetch(routeEdit, {
+        const deleteResponse = await fetch(routeEdit, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({valueId: dataId}),
         });
+        // console.log(deleteResponse)
+        // console.log(deleteResponse.body)
+        const response = await deleteResponse.json();
+        // console.log(JSON.stringify(response))
+        document.getElementById("user_order_table").innerHTML = response.html;
+        deleteItem()
+
     }
     catch (e) {
-        console.error('Error while creating item order')
+        console.error('Error while deleting item order')
+        console.log(e)
+    }
+}
+async function deleteItem(){
+    const btns = document.getElementsByClassName("btn-delete-order-item");
+    for (const btn of btns) {
+        btn.addEventListener('click', () => {
+                alert('clicked')
+                const dataId = btn.getAttribute("data-id");
+                deleteOrderItem('http://localhost:8080/user_order/delete', dataId)
+            }
+            , false);
     }
 }
 
-const btns = document.getElementsByClassName("btn-delete-order-item");
-for (const btn of btns) {
-    btn.addEventListener('click', () => {
-        alert('clicked')
-            const dataId = btn.getAttribute("data-id");
-            deleteOrderItem('http://localhost:8080/user_order/delete', dataId)
-        }
-        , false);
-}
+deleteItem()
--- app/src/Controller/UserOrderController.php
+++ app/src/Controller/UserOrderController.php
@@ -6,9 +6,11 @@ use App\Entity\ItemOrder;
 use App\Service\ItemOrderService;
 use App\Service\UserOrderService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\VarDumper\VarDumper;
 
 class UserOrderController extends AbstractController
 {
@@ -23,16 +25,22 @@ class UserOrderController extends AbstractController
 
 
     #[Route('/user_order/delete', name: 'order_list_delete', methods: ['DELETE', 'POST'])]
-    public function delete(Request $request, ItemOrderService $itemOrderService)
+    public function delete(Request $request, ItemOrderService $itemOrderService, UserOrderService $service)
     {
         $parameters = json_decode($request->getContent(), true);
         $itemOrderId = $parameters['valueId'];
         $itemOrder = $itemOrderService->find($itemOrderId);
         $itemOrderService->delete($itemOrder);
 
-        $response = new Response();
-        $response->setStatusCode(Response::HTTP_OK);
-        $response->send();
+        $render = $this->renderView('user_order/user-order-table.html.twig', [
+            'userOrder' => $service->findUserOrders(),
+        ]);
+//        VarDumper::dump($render);
+        return new JsonResponse(['html' => $render
+        ]);
 
+
+        // return $this->renderView('user_order/user-order-table.html.twig', array('userOrder' => $service->findUserOrders()));
+//        VarDumper::dump($test);exit;
     }
 }
