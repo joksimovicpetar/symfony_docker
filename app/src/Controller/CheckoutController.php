700ffc08c9ba3d86b75eba8096cb99b13c2045a1
joksimovicpetar <petar.d.joksimovic@gmail.com>
2022-11-03 16:15:25 +0100

    order commit

--- app/src/Controller/CheckoutController.php
+++ app/src/Controller/CheckoutController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\UserOrderService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class CheckoutController extends AbstractController
 {
     #[Route('/checkout', name: 'app_checkout')]
-    public function index(): Response
+    public function findUserOrders(UserOrderService $service): Response
     {
+
         return $this->render('checkout/index.html.twig', [
-            'controller_name' => 'CheckoutController',
+            'userOrder' => $service->findUserOrders(),
         ]);
     }
 }
--- app/src/Controller/ExtraIngredientController.php
+++ app/src/Controller/ExtraIngredientController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Service\ExtraIngredientService;
+use App\Service\UserOrderService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,12 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class ExtraIngredientController extends AbstractController
 {
     #[Route('/extra_ingredient', name: 'extra_ingredient_list', methods: 'GET')]
-    public function index(ExtraIngredientService $service): Response
+    public function index(ExtraIngredientService $service, UserOrderService $userOrderService): Response
     {
         $extraIngredients = $service->findExtraIngredients();
 
         return $this->render('extra_ingredient/index.html.twig', [
             'extraIngredients' => $extraIngredients,
+            'userOrder' => $userOrderService->findUserOrders(),
         ]);
     }
 }
