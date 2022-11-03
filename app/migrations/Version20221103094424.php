9d5c1a6a2655d5e2f5fb0eba9c971207a663c893
joksimovicpetar <petar.d.joksimovic@gmail.com>
2022-11-03 13:33:34 +0100

    order commit

---
+++ app/migrations/Version20221103094424.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221103094424 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('
+            ALTER TABLE item_order
+                  ADD totalPrice FLOAT;
+            
+        ');
+    }
+
+    public function down(Schema $schema): void
+    {
+
+    }
+}
